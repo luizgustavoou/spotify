daee712af6de2e820896a857a11293b5b6ef8935
luizgustavoou <luiz.umbelino@nuplam.ufrn.br>
2023-11-26 19:20:09 -0300

    feature(GetAllSongsOfplaylist): injetar dependencia

--- src/main/java/imd/ufrn/br/spotify/services/GetAllSongsOfPlaylistUseCaseImpl.java
+++ src/main/java/imd/ufrn/br/spotify/services/GetAllSongsOfPlaylistUseCaseImpl.java
@@ -1,11 +1,27 @@
 package imd.ufrn.br.spotify.services;
 
 import imd.ufrn.br.spotify.entities.Song;
+import imd.ufrn.br.spotify.services.folder.IFindAllSongsOfFolderUseCase;
+import imd.ufrn.br.spotify.services.folder.impl.FindAllSongsOfFolderUseCaseImpl;
+import imd.ufrn.br.spotify.services.song.IFindAllSongsOfPlaylist;
+import imd.ufrn.br.spotify.services.song.impl.FindAllSongsOfPlaylistImpl;
 
 import java.util.List;
 import java.util.UUID;
 
 public class GetAllSongsOfPlaylistUseCaseImpl implements IGetAllSongsOfPlaylistUseCase{
+    IFindAllSongsOfFolderUseCase findAllSongsOfFolderUseCase;
+    IFindAllSongsOfPlaylist findAllSongsOfPlaylist;
+
+    GetAllSongsOfPlaylistUseCaseImpl(IFindAllSongsOfFolderUseCase findAllSongsOfFolderUseCase, IFindAllSongsOfPlaylist findAllSongsOfPlaylist) {
+        this.findAllSongsOfFolderUseCase = findAllSongsOfFolderUseCase;
+        this.findAllSongsOfPlaylist = findAllSongsOfPlaylist;
+    }
+    GetAllSongsOfPlaylistUseCaseImpl() {
+        this.findAllSongsOfFolderUseCase = new FindAllSongsOfFolderUseCaseImpl();
+        this.findAllSongsOfPlaylist = new FindAllSongsOfPlaylistImpl();
+    }
+
     @Override
     public List<Song> execute(UUID playlistId) {
         return null;
