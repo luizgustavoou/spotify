875793cf8152bb91a2009887bc08963674e8db02
luizgustavoou <luizgustavooumbelino@gmail.com>
2023-12-12 10:14:56 -0300

    feature(FolderApi): implementar

--- src/main/java/imd/ufrn/br/spotify/front/apis/impl/FolderApiImpl.java
+++ src/main/java/imd/ufrn/br/spotify/front/apis/impl/FolderApiImpl.java
@@ -1,5 +1,6 @@
 package imd.ufrn.br.spotify.front.apis.impl;
 
+import imd.ufrn.br.spotify.back.controllers.FolderController;
 import imd.ufrn.br.spotify.back.entities.Folder;
 import imd.ufrn.br.spotify.exceptions.EntityNotFoundException;
 import imd.ufrn.br.spotify.front.apis.IFolderApi;
@@ -7,13 +8,19 @@ import imd.ufrn.br.spotify.front.apis.IFolderApi;
 import java.util.UUID;
 
 public class FolderApiImpl implements IFolderApi {
+    FolderController folderController;
+
+    public FolderApiImpl() {
+        this.folderController = new FolderController();
+    }
+
     @Override
     public Folder create(String path, UUID playlistId) {
-        return null;
+        return this.folderController.create(path, playlistId);
     }
 
     @Override
     public void remove(UUID id) throws EntityNotFoundException {
-
+        this.folderController.remove(id);
     }
 }
