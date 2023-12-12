0cd1578ec5fa5cd0a278f431c36f7ae6d10e29a2
luizgustavoou <luizgustavooumbelino@gmail.com>
2023-12-12 10:16:59 -0300

    feature(PlaylistApi / SongApi): implementar

--- src/main/java/imd/ufrn/br/spotify/front/apis/impl/PlaylistApiImpl.java
+++ src/main/java/imd/ufrn/br/spotify/front/apis/impl/PlaylistApiImpl.java
@@ -1,5 +1,6 @@
 package imd.ufrn.br.spotify.front.apis.impl;
 
+import imd.ufrn.br.spotify.back.controllers.PlaylistController;
 import imd.ufrn.br.spotify.back.entities.Playlist;
 import imd.ufrn.br.spotify.exceptions.EntityNotFoundException;
 import imd.ufrn.br.spotify.front.apis.IPlaylistApi;
@@ -7,18 +8,24 @@ import imd.ufrn.br.spotify.front.apis.IPlaylistApi;
 import java.util.UUID;
 
 public class PlaylistApiImpl implements IPlaylistApi {
+    PlaylistController playlistController;
+
+    public PlaylistApiImpl() {
+        this.playlistController = new PlaylistController();
+
+    }
     @Override
     public Playlist create(String name, UUID userId) {
-        return null;
+        return this.playlistController.create(name, userId);
     }
 
     @Override
     public void remove(UUID id) throws EntityNotFoundException {
-
+        this.playlistController.remove(id);
     }
 
     @Override
     public void update(UUID id, String name, UUID userId) throws EntityNotFoundException {
-
+        this.playlistController.update(id, name, userId);
     }
 }
--- src/main/java/imd/ufrn/br/spotify/front/apis/impl/SongApiImpl.java
+++ src/main/java/imd/ufrn/br/spotify/front/apis/impl/SongApiImpl.java
@@ -1,5 +1,6 @@
 package imd.ufrn.br.spotify.front.apis.impl;
 
+import imd.ufrn.br.spotify.back.controllers.SongController;
 import imd.ufrn.br.spotify.back.entities.Song;
 import imd.ufrn.br.spotify.exceptions.EntityNotFoundException;
 import imd.ufrn.br.spotify.front.apis.ISongApi;
@@ -7,18 +8,23 @@ import imd.ufrn.br.spotify.front.apis.ISongApi;
 import java.util.UUID;
 
 public class SongApiImpl implements ISongApi {
+    SongController songController;
+
+    public SongApiImpl() {
+        this.songController = new SongController();
+    }
     @Override
     public Song create(String name, String path, UUID playlistId) {
-        return null;
+        return this.songController.create(name, path, playlistId);
     }
 
     @Override
     public void remove(UUID id) throws EntityNotFoundException {
-
+        this.songController.remove(id);
     }
 
     @Override
     public void update(UUID id, String name, String path, UUID playlistId) throws EntityNotFoundException {
-
+        this.songController.update(id, name, path, playlistId);
     }
 }
