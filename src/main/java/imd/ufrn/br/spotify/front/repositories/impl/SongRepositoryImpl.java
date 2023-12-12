4e3814be06ec5c557230cd045ffe0f8e00f9f79c
luizgustavoou <luizgustavooumbelino@gmail.com>
2023-12-12 10:26:51 -0300

    feature(SongRepositoryImpl): implementar

--- src/main/java/imd/ufrn/br/spotify/front/repositories/impl/SongRepositoryImpl.java
+++ src/main/java/imd/ufrn/br/spotify/front/repositories/impl/SongRepositoryImpl.java
@@ -1,4 +1,28 @@
 package imd.ufrn.br.spotify.front.repositories.impl;
 
+import imd.ufrn.br.spotify.back.entities.Song;
+import imd.ufrn.br.spotify.exceptions.EntityNotFoundException;
+import imd.ufrn.br.spotify.front.apis.ISongApi;
+import imd.ufrn.br.spotify.front.apis.impl.SongApiImpl;
+
+import java.util.UUID;
+
 public class SongRepositoryImpl {
+    private final ISongApi songApi;
+
+    public SongRepositoryImpl() {
+        this.songApi = new SongApiImpl();
+    }
+
+    public Song create(String name, String path, UUID playlistId) {
+        return this.songApi.create(name, path, playlistId);
+    }
+
+    public void remove(UUID id) throws EntityNotFoundException {
+        this.songApi.remove(id);
+    }
+
+    public void update(UUID id, String name, String path, UUID playlistId) throws EntityNotFoundException {
+        this.songApi.update(id, name, path, playlistId);
+    }
 }
